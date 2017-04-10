ba1e2bb657b0e516137cc3a9c580315407b6d590
Mohit Sharma <mohit.sharma@TECH-MOHITSHARMA.local>
2017-04-10 17:26:53 +0530

    Initial commit

---
+++ app/src/main/java/dth/online/com/firebase/MainActivity.java
@@ -0,0 +1,15 @@
+package dth.online.com.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
